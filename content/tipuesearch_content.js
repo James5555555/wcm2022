97226ab6b971647eba39e05c7535a01198c23a74
james <51033117@gm.nfu.edu.tw>
2022-06-26 21:19:21 +0800

    james

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n \n \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'UK', 'text': '\n', 'tags': '', 'url': 'UK.html'}]};
